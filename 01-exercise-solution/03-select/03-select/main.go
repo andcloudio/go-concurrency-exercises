626ff40651aab0e6d05ccc0a75298e9d7eef9d15
dgunjetti <deepakgb79@gmail.com>
2020-09-15 11:12:40 +0530

    update

--- 01-exercise-solution/03-select/03-select/main.go
+++ 01-exercise-solution/03-select/03-select/main.go
@@ -9,17 +9,24 @@ func main() {
 	ch := make(chan string)
 
 	go func() {
-		time.Sleep(1 * time.Second)
-		ch <- "one"
+		for i := 0; i < 3; i++ {
+			time.Sleep(1 * time.Second)
+			ch <- "message"
+		}
+
 	}()
 
 	// if there is no value on channel, do not block.
+	for i := 0; i < 2; i++ {
+		select {
+		case m := <-ch:
+			fmt.Println(m)
+		default:
+			fmt.Println("no message received")
+		}
 
-	select {
-	case m := <-ch:
-		fmt.Println(m)
-	default:
-		fmt.Println("no message in channel")
+		// Do some processing..
+		fmt.Println("processing..")
+		time.Sleep(1500 * time.Millisecond)
 	}
-
 }
--- 01-exercise/03-select/03-select/main.go
+++ 01-exercise/03-select/03-select/main.go
@@ -9,13 +9,20 @@ func main() {
 	ch := make(chan string)
 
 	go func() {
-		time.Sleep(1 * time.Second)
-		ch <- "one"
+		for i := 0; i < 3; i++ {
+			time.Sleep(1 * time.Second)
+			ch <- "message"
+		}
+
 	}()
 
 	// TODO: if there is no value on channel, do not block.
+	for i := 0; i < 2; i++ {
+		m := <-ch
+		fmt.Println(m)
 
-	m := <-ch
-	fmt.Println(m)
-
+		// Do some processing..
+		fmt.Println("processing..")
+		time.Sleep(1500 * time.Millisecond)
+	}
 }
