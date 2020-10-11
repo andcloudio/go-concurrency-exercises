1055680d6a1ce3c24c5125e1b63677aec6294394
dgunjetti <deepakgb79@gmail.com>
2020-10-11 06:18:36 +0530

    update

--- 01-exercise-solution/01-goroutines/04-add/main.go
+++ 01-exercise-solution/01-goroutines/04-add/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"go-concurrency-exercises/01-exercise/01-goroutines/04-add/counting"
+	"go-concurrency-exercises/01-exercise-solution/01-goroutines/04-add/counting"
 )
 
 func main() {
