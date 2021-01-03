355a1ce82993cfcade696dfd0935cbf561342fc0
Uriel Márquez <um@c0d3.mx>
2021-01-03 11:57:30 +0000

    ex. 1 solved

---
+++ 01-exercise/07-exercise-web-crawler/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.14
+
+require golang.org/x/net v0.0.0-20201224014010-6772e930b67b
--- 02-exercise/02-pipeline/01-pipeline/main.go
+++ 02-exercise/02-pipeline/01-pipeline/main.go
@@ -1,25 +1,69 @@
 package main
+import (
+  "math/rand"
+  "sync"
+  "fmt"
+  "time"
+)
 
 // TODO: Build a Pipeline
 // generator() -> square() -> print
 
 // generator - convertes a list of integers to a channel
-func generator(nums ...int) {
+func generator(nums ...int)  <-chan int {
+  out := make(chan int, len(nums))
 
+  go func(){
+    for _, n := range nums {
+      fmt.Printf("1)\t saving %v\n", n)
+      out <- n
+    }
+
+    close(out)
+  }()
+
+  return out
 }
 
 // square - receive on inbound channel
 // square the number
 // output on outbound channel
-func square() {
+func square(chIn <-chan int) <-chan int {
+  out := make(chan int)
 
+  go func(){
+    for n := range chIn {
+      fmt.Printf("2)\t %v*%v = %v\n", n, n, n*n)
+      out <- n*n
+    }
+
+    close(out)
+  }()
+
+  return out
 }
 
 func main() {
+  start := time.Now()
 	// set up the pipeline
+  vals := []int{}
+  for i := 0; i < 100; i++ {
+    vals = append(vals, rand.Intn(99)+1)
+  }
 
 	// run the last stage of pipeline
 	// receive the values from square stage
 	// print each one, until channel is closed.
+  wg := sync.WaitGroup{}
+  wg.Add(1)
+  go func(){
+    for v := range square(generator(vals...)) {
+      fmt.Printf("3)\t result: %v\n", v)
+    }
 
+    wg.Done()
+  }()
+
+  wg.Wait()
+  fmt.Printf("elapsed time: %vms\n", time.Since(start).Milliseconds())
 }
