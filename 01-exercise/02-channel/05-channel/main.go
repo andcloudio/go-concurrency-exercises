9ace4cd354d52ac67c33be05b49d4fb4cee7e7fa
Navaneeth <32714420+neeth04@users.noreply.github.com>
2023-04-21 14:48:11 -0700

    channels

--- 01-exercise/02-channel/05-channel/main.go
+++ 01-exercise/02-channel/05-channel/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//TODO: create channel owner goroutine which return channel and
-	// writes data into channel and
-	// closes the channel when done.
+
+	owner := func() <-chan int {
+		ch := make(chan int)
+
+		go func() {
+			defer close(ch)
+			for i := 0; i < 6; i++ {
+				ch <- i
+			}
+		}()
+		return ch
+	}
 
 	consumer := func(ch <-chan int) {
 		// read values from channel
