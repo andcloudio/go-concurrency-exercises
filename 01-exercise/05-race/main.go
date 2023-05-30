01ee38dd0d49c94aeea53a520887bfde4124c08e
Navaneeth <32714420+neeth04@users.noreply.github.com>
2023-05-30 16:37:42 -0700

    identify race

--- 01-exercise/05-race/main.go
+++ 01-exercise/05-race/main.go
@@ -6,16 +6,19 @@ import (
 	"time"
 )
 
-//TODO: identify the data race
-// fix the issue.
-
 func main() {
 	start := time.Now()
 	var t *time.Timer
+	ch := make(chan bool)
 	t = time.AfterFunc(randomDuration(), func() {
 		fmt.Println(time.Now().Sub(start))
-		t.Reset(randomDuration())
+		ch <- true
 	})
+	for time.Since(start) < 5 * time.Second {
+		<- ch
+		t.Reset(randomDuration())
+	}
+	
 	time.Sleep(5 * time.Second)
 }
 
